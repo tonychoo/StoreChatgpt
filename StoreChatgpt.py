03a81bc2d6c5c47eff076774534859d782b03edb
tonychoo <coolax@gmail.com>
2023-01-13 13:13:33 +0800

    Update prompt for inputs.

--- StoreChatgpt.py
+++ StoreChatgpt.py
@@ -3,20 +3,26 @@ import openai
 # Use your OpenAI API key
 openai.api_key = "{APIKey}"
 
-# The message you want to send to ChatGPT
-message = "{Message}"
+while True:
+    message = input("You: ")
+    if message.lower() == "exit":
+        break
+    # Send the message to ChatGPT and receive the response
+    try:
+        response = openai.Completion.create(
+                engine="text-davinci-002",
+                prompt=message,
+                max_tokens=2048,
+                n = 1,
+                stop=None,
+                temperature=0.5,
+                timeout=10
+            )
+        # Extract the ChatGPT's response from the JSON object
+        chatgpt_response = response["choices"][0]["text"].strip()
+        print("ChatGPT: " + chatgpt_response)
+    except openai.exceptions.OpenAiError as e:
+        print("Error: ", e)
+        break
 
-# Send the message to ChatGPT and receive the response
-response = openai.Completion.create(
-    engine="text-davinci-002",
-    prompt=message,
-    max_tokens=2048,
-    n = 1,
-    stop=None,
-    temperature=0.5
-)
-
-# Extract the ChatGPT's response from the JSON object
-chatgpt_response = response["choices"][0]["text"].strip()
-
-print(chatgpt_response)
+    print("ChatGPT: " + chatgpt_response)
